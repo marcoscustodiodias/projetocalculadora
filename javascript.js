bb313817d3bba926d991c2979309f9fa3c3615e7
Marcos Dias <marcosdias.pvai@gmail.com>
2022-05-29 17:20:26 -0300

    feat: adiciona a lógica da calculadora

---
+++ javascript.js
@@ -0,0 +1,74 @@
+let n1 = Number(prompt(('Insira o primeiro valor:')));
+let n2 = Number(prompt(('Insira o segundo valor:')));
+let resultado;
+
+
+function soma() {
+    resultado = n1 + n2;
+    alert(`${n1} + ${n2} = ${resultado}`);
+    novaoperacao();
+}
+
+function subtracao() {
+    resultado = n1 - n2;
+    alert(`${n1} - ${n2} = ${resultado}`);
+    novaoperacao();
+}
+
+function multiplicacao() {
+    resultado = n1 * n2;
+    alert(`${n1} * ${n2} = ${resultado}`);
+    novaoperacao();
+}
+
+function divisao() {
+    resultado = n1 / n2;
+    alert(`${n1} / ${n2} = ${resultado}`);
+    novaoperacao();
+}
+
+function divisaoInteira() {
+    resultado = n1 % n2;
+    alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
+    novaoperacao();
+}
+
+function potenciacao() {
+    resultado = n1 ** n2;
+    alert(`${n1} elevado a ${n2}ª ígual a ${resultado}`);
+    novaoperacao();
+}
+
+function novaoperacao() {
+    let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+
+    if (opcao == 1) {
+        calculadora();
+     } else if (opcao == 2) {
+        alert('Ate mais!')
+    } else {
+        alert('digite uma opção valida!')
+    }
+    novaoperacao();
+}
+
+function calculadora () {
+    const operacao = Number(prompt('Escolha sua operação:\n 1 - Soma (+) \n 2 - Subtração (-) \n 3 - Multiplicação (*) \n 4 - Divisão (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)'));
+    if (operacao == 1){
+        soma();
+    } else if (operacao == 2){
+        subtracao();
+    } else if (operacao == 3){
+        multiplicacao();
+    } else if (operacao == 4){
+        divisao();
+    } else if (operacao == 5){
+        divisaoInteira();
+    } else if (operacao == 6){
+        potenciacao();
+    }
+}
+
+
+calculadora();
+
